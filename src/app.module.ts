af98061213cd90c7b5e0a3467f218332d93118ce
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-01-30 18:43:53 -0600

    init langchain once only

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssessmentsController } from './assessments/assessments.controller';
 import { AssessmentsService } from './assessments/assessments.service';
-import { LangchainService } from './langchain/langchain.service';
 import { LangchainModule } from './langchain/langchain.module';
 import { AssessmentsModule } from './assessments/assessments.module';
 
 @Module({
   imports: [LangchainModule, AssessmentsModule],
   controllers: [AppController, AssessmentsController],
-  providers: [AppService, AssessmentsService, LangchainService],
+  providers: [AppService, AssessmentsService],
 })
 export class AppModule {}
--- src/assessments/assessments.service.ts
+++ src/assessments/assessments.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { LangchainService } from 'src/langchain/langchain.service';
 
 @Injectable()
 export class AssessmentsService {
-  constructor(private readonly langchain: LangchainService) {
-    // console.log('AssessmentsService constructor created');
-  }
+  constructor() {}
 
   getAssessments(): string {
     return 'This will return all assessments';
