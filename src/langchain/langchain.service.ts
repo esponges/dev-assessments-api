b2f5c0c576558cfdb7a2df84902530b95674d4e3
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-01-30 19:11:52 -0600

    Add ChatOpenAI integration and helper functions

--- src/langchain/langchain.service.ts
+++ src/langchain/langchain.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { ChatOpenAI } from '@langchain/openai';
+import { createStructuredOutputRunnable } from 'langchain/chains/openai_functions';
+import { JsonOutputFunctionsParser } from 'langchain/output_parsers';
+import {
+  ChatPromptTemplate,
+  type BaseMessagePromptTemplateLike,
+} from '@langchain/core/prompts';
+import { InputValues } from '@langchain/core/utils/types';
 
 @Injectable({})
 export class LangchainService {
+  private chatOpenAI: ChatOpenAI = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: 'gpt-4',
+  });
+  private outputParser = new JsonOutputFunctionsParser();
+  private someRandomPrompt = ChatPromptTemplate.fromMessages([
+    ['human', 'Question: {question}'],
+  ]);
+
   constructor() {
     console.log('LangchainService constructor created');
+  }
+
+  generatePrompt(
+    promptMessages: (
+      | ChatPromptTemplate<InputValues, string>
+      | BaseMessagePromptTemplateLike
+    )[],
+  ) {
+    const prompt = ChatPromptTemplate.fromMessages(promptMessages);
+    return prompt;
+  }
+
+  async getRunnable(
+    schema: Record<string, string>,
+    prompt: typeof this.someRandomPrompt, // fix this hack
+  ) {
+    const runnable = createStructuredOutputRunnable({
+      outputSchema: schema,
+      llm: this.chatOpenAI,
+      prompt,
+      outputParser: this.outputParser,
+    });
+
+    return runnable;
   }
 }
