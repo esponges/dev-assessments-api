e9ff9ab72286aaf8281c258b798f36e3afa2c139
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-06 10:39:51 -0600

    remove unnecessary client import

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,16 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { Pinecone } from '@pinecone-database/pinecone';
 import { PineconeService } from 'src/pinecone/pinecone.service';
 import { GetQuestionsDto } from './dto/get-questions';
 
 @Controller('questions')
 export class QuestionsController {
-  // todo: probably won't need this service
-  private readonly pineconeClient: Pinecone;
-
-  constructor(private readonly pineconeService: PineconeService) {
-    this.pineconeClient = pineconeService.getClient();
-  }
+  constructor(private readonly pineconeService: PineconeService) {}
 
   @Post()
   async getQuestions(@Body() getQuestionsDto: GetQuestionsDto) {
