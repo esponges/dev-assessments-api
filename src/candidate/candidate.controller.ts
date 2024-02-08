7f18a238770edf0489a1682848aac9566b5b8fa4
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-08 11:17:05 -0600

    accept file uploads

--- src/candidate/candidate.controller.ts
+++ src/candidate/candidate.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CandidateService } from './candidate.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('candidate')
 export class CandidateController {
   constructor(private readonly candidateService: CandidateService) {}
 
   @Post('parse_resume')
-  parseResume() {
+  @UseInterceptors(FileInterceptor('file'))
+  parseResume(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
     return this.candidateService.parseResume();
   }
 }
