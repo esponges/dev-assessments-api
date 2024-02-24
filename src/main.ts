f518488cdc96a305d4c13757ee9b43b7e68410e0
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-24 11:21:57 -0600

    enable cors for preflight reqs

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,19 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://example.com',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+    ],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(9000);
 }
 bootstrap();
