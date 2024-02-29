caf60dc032959d10e8b350919ac15398bf57bab8
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-29 12:16:08 -0600

    renamings

--- src/assessments/assessments.controller.ts
+++ src/assessments/assessments.controller.ts
@@ -4,6 +4,7 @@ import { Controller } from '@nestjs/common';
 import { AssessmentsService } from './assessments.service';
 import { CreateAssessmentDto } from './dto/create-assessment.dto';
 import { EvaluateChallengeDto } from './dto/evaluate-challenge.dto';
+import { EvaluateAssessmentDto } from './dto/evaluate-assessment.dto';
 
 @Controller('assessments')
 export class AssessmentsController {
@@ -19,12 +20,17 @@ export class AssessmentsController {
     return this.assessmentsService.createAssessment(createAssessmentBody);
   }
 
-  @Post('evaluate_challenge')
+  @Post('evaluate/challenge')
   evaluateChallenge(@Body() evaluateAssessmentBody: EvaluateChallengeDto) {
     return this.assessmentsService.evaluateChallenge({
       // todo: this replace is a temporary workaround to send the response from postman
       devResponse: evaluateAssessmentBody.devResponse.replaceAll(`'`, `"`),
       ...evaluateAssessmentBody,
     });
+  }
+
+  @Post('evaluate/questions')
+  evaluateAssessment(@Body() evaluateAssessmentBody: EvaluateAssessmentDto) {
+    console.log(evaluateAssessmentBody);
   }
 }
