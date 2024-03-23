ebf8b9a99c858119ba129b152d43823ce5142c9b
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-03-23 13:54:34 -0600

    get headers from req

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Headers } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './folder/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('webhook')
+  async webhook(@Headers() headers: any) {
+    return headers;
+  }
 
   @Post('upsert')
   async upsert(@Body() body: UserDto) {
